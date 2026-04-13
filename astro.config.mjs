0b88af2fb0b67fbc74f18d20c8e9616bee9d0de8
san-serif <dshum17@hotmail.com>
2026-04-13 23:40:30 +1000

    site: add github action for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://san-serif.github.io',
+  base: '/', // Change this to '/your-repo-name' if it's not a primary domain
+});
