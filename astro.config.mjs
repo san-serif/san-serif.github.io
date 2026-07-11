d6c5129620cb0fba53a98e44abc706af02db6c23
san-serif <dshum17@hotmail.com>
2026-07-11 14:52:38 +1000

    Added metadata to improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://san-serif.github.io',
   base: '/', // Change this to '/your-repo-name' if it's not a primary domain
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
