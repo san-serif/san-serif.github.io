1978468502003c915ac44ae282b6babbaa44a77d
san-serif <dshum17@hotmail.com>
2026-07-26 09:24:02 +1000

    Point site at san-serif.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://san-serif.github.io',
+  site: 'https://san-serif.com',
   base: '/', // Change this to '/your-repo-name' if it's not a primary domain
   integrations: [mdx(), sitemap()],
 });
